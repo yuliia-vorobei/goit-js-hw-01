fb314fee8bfb40af868ad7c3f3423e19378134e5
Yuliia Vorobei <vorobeju1@gmail.com>
2024-04-26 20:16:24 +0200

    java script 1 homework

---
+++ js/task-1.js
@@ -0,0 +1,6 @@
+function makeTransaction(quantity, pricePerDroid) {
+  return `You ordered ${quantity} droids worth ${pricePerDroid} credits!`;
+}
+console.log(makeTransaction(5, 3000));
+console.log(makeTransaction(3, 1000));
+console.log(makeTransaction(10, 500));
